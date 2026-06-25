6582bf063ff276c5e1f27b2385ada803e2f11f9e
Ana <ana.c.santos611@aluno.senai.br>
2026-06-25 17:00:00 -0300

    salvando

---
+++ js/scripts_calculos.js
@@ -0,0 +1,31 @@
+export function calcularIPVA(valorVeiculo, anoFabricacao, combustivel) {
+    const anoAtual = new Date().getFullYear();
+    const idadeVeiculo = anoAtual - anoFabricacao;
+
+    if (idadeVeiculo > 20) {
+        return 0;
+    } else {
+        let percentual;
+
+        if (combustivel === "gasolina") {
+            percentual = 0.20;
+        } else if (combustivel === "etanol") {
+            percentual = 0.15;
+        } else if (combustivel === "bicombustivel") {
+            percentual = 0.10;
+        } else if (combustivel === "hibrido") {
+            percentual = 0.08;
+        } else if (combustivel === "eletrico") {
+            percentual = 0.02;
+        } else {
+            percentual = 0;
+        }
+
+        return valorVeiculo * percentual;
+    }
+}
+
+export function calcularSeguro(valorVeiculo) {
+    return valorVeiculo * 0.10;
+}
+
---
+++ js/scripts_veiculos.js
@@ -0,0 +1,77 @@
+import { calcularIPVA, calcularSeguro } from "./script_calculos.js";
+
+
+const form = document.querySelector("#formVeiculo");
+const lista = document.querySelector("#listaVeiculos");
+
+let veiculos = [];
+
+form.addEventListener("submit", (evt) => {
+
+    evt.preventDefault();
+
+    const marca = document.querySelector("#marca").value;
+    const modelo = document.querySelector("#modelo").value;
+    const placa = document.querySelector("#placa").value;
+    const ano = Number (document.querySelector("#ano").value);
+    const valor = Number (document.querySelector("#valor").value);
+
+    const combustivel =
+        document.querySelector('input[name="combustivel"]:checked').value;
+
+    const veiculo = {
+        marca,
+        modelo,
+        placa,
+        ano,
+        valor,
+        combustivel
+    };
+
+    veiculos.push(veiculo);
+
+    listarVeiculos();
+
+    form.reset();
+});
+
+function listarVeiculos(){
+
+    const anoAtual = new Date().getFullYear();
+
+    lista.innerHTML = veiculos.map((veiculo) => {
+
+        const idade = anoAtual - veiculo.ano;
+
+       const ipva = calcularIPVA(
+        veiculo.valor,
+        veiculo.ano,
+        veiculo.combustivel)
+    
+        const seguro = calcularSeguro(veiculo.valor);
+        const licenciamento = 200;
+
+        const valorFinal = ipva + seguro + licenciamento;
+
+        return `
+            <div class="card">
+
+                <h3>${veiculo.modelo}</h3>
+
+                Marca: ${veiculo.marca}<br>
+                Placa: ${veiculo.placa}<br>
+                Idade: ${idade} anos<br>
+
+                Seguro: R$ ${seguro.toFixed(2)}<br>
+                IPVA: R$ ${ipva.toFixed(2)}<br>
+                Licenciamento: R$ ${licenciamento.toFixed(2)}<br>
+
+            
+                    Valor Final: R$ ${valorFinal.toFixed(2)}<br>
+                
+
+            </div>
+        `;
+    })
+}
+
